b3c77ea71bbf9695c4b167bfd6b9ee480fe80874
PingGENO <53820716+PingGENO@users.noreply.github.com>
2022-11-03 00:55:37 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#$ -cwd -N CD4_C1 -P jknight.prjc -q short.qc
+#$ -cwd -N exon -P {group}.prjc -q short.qc
 #$ -j y -b n 
 #$ -pe shmem 12
-#$ -o /users/jknight/kwz374/R/AS.patinets.splicing
-#$ -e /users/jknight/kwz374/R/AS.patinets.splicing
 
 # Make the script stop on any error.
 set -uxeo pipefail
@@ -11,6 +9,6 @@ set -uxeo pipefail
 echo "job started on" `date +"%T %Y-%m-%d"`
 module load R/3.6.2-foss-2019b
 
-Rscript splicing_CD4.r fcount.DEXSeq_AS.patients_CD4_Cohort1.and.2.txt
+Rscript splicing_CD4.r fcount.DEXSeq.no.r.txt
 
 echo "job finished on" `date +"%T %Y-%m-%d"`
