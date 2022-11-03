855e76939af2a4a3dcf62e999e6cfad490f0c71b
PingGENO <53820716+PingGENO@users.noreply.github.com>
2022-11-03 08:57:53 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ set -uxeo pipefail
 echo "job started on" `date +"%T %Y-%m-%d"`
 module load R/3.6.2-foss-2019b
 
-Rscript splicing_CD4.r fcount.DEXSeq.no.r.txt
+Rscript splicing_CD8.r fcount.DEXSeq.no.r.txt
 
 echo "job finished on" `date +"%T %Y-%m-%d"`
